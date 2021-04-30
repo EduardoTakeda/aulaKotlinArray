34af991cdfc24e21b35a526d95f6130b9b347768
Eduardo Takeda <eduardotakeda11@yahoo.com.br>
2021-04-30 18:28:59 -0300

    Aula Kotlin Funcoes Extendidas Testes Colecoes, funcoes, mutaveis

--- src/main/kotlin/TesteSet.kt
+++ src/main/kotlin/TesteSet.kt
@@ -1,6 +1,6 @@
 fun main() {
     val joao = Funcionario(nome = "Joao", salario=3000.0, tipoContratacao="CLT")
-    val pedro=Funcionario(nome = "Pedro", salario=2000.0, tipoContratacao="PJ")
+    val pedro=Funcionario(nome = "Pedro", salario=1500.0, tipoContratacao="PJ")
     val maria=Funcionario(nome = "Maria", salario=4000.0, tipoContratacao="CLT")
 
     val funcionarios1 = setOf(joao, pedro)
